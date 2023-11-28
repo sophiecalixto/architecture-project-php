9e67a217c92c04e9d13686f095b6cd57c96c39e1
Sophie Calixto <sophiecalixto2004@gmail.com>
2023-11-28 17:56:34 -0300

    feat: create student by static method in class Student

--- src/Student.php
+++ src/Student.php
@@ -8,7 +8,13 @@ class Student
     private Document $document;
     private Email $email;
     private array $phones;
-    public function __construct(string $name, Document $document, Email $email, array $phones)
+
+    public static function withNameDocumentAndEmail($name, $document, $email) : self
+    {
+        return new Student($name, new Document($document), new Email($email));
+    }
+
+    public function __construct(string $name, Document $document, Email $email)
     {
 
         $this->name = $name;
@@ -16,8 +22,9 @@ class Student
         $this->email = $email;
     }
 
-    public function addPhone(string $country_number, string $ddd, string $number) : void
+    public function addPhone(string $country_number, string $ddd, string $number) : self
     {
         $this->phones[] = new Phone($country_number, $ddd, $number);
+        return $this;
     }
 }
